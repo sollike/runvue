dce5ce67a5f78d86737f7a36350025413f2f76e0
Sol <0okacii@naver.com>
2021-08-02 02:40:32 +0900

    ex1 컴포지션으로 만들어보기

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <template>
-  <h1 @click="increase">
-    {{ count }} / {{ doubleCount }}
-  </h1>
-  <h1>
-    {{ message }} / {{ reversedMessage }}
-  </h1>
+  <div @click="increase">
+    {{ count }}
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
-  data() {
+  setup() { //여기서 정의한 변수는 반응성을 가지지 않음
+    let count = ref(0) // 반응성을 가진 객체 데이터로 만들어줌
+    function increase() {
+      count.value += 1 //객체가 되었기 때문에 value로 접근
+    }
+
     return {
-      message: 'Hello world!',
-      count: 0
-    }
-  },
-  computed: {
-    doubleCount() {
-      return this.count * 2
-    },
-    reversedMessage() {
-      return this.message.split('').reverse().join('')
-    }
-  },
-  methods: {
-    increase() {
-      this.count += 1
+      count,
+      increase
     }
   }
 }
